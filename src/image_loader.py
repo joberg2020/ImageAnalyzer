7123e7c99cfe281ad50f4e203c908d20c636a1d2
Jörgen Öberg <jo223cy@student.lnu.se>
2023-12-06 20:43:33 +0100

    Green

--- src/image_loader.py
+++ src/image_loader.py
@@ -1,6 +1,18 @@
 from PIL import Image
+import io
+import requests
 
-class ImageLoader():
+class ImageLoader:
   
   def load_from_url(self, url: str) -> Image.Image:
-    pass
+    """
+    Load an image from a URL.
+    """
+    response = requests.get(url)
+    response.raise_for_status()
+
+    image_stream = io.BytesIO(response.content)
+    image = Image.open(image_stream)
+    image.load()
+
+    return image
