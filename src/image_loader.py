f581806abe86f733e59d3149bb76a7b69ecc8d14
Jörgen Öberg <jo223cy@student.lnu.se>
2023-12-06 01:42:20 +0100

    Red

---
+++ src/image_loader.py
@@ -0,0 +1,6 @@
+from PIL import Image
+
+class ImageLoader():
+  
+  def load_from_url(self, url: str) -> Image.Image:
+    pass
---
+++ tests/test_image_loader.py
@@ -0,0 +1,19 @@
+from unittest.mock import patch, MagicMock
+from src.image_loader import ImageLoader
+from PIL import Image
+import pytest
+import io
+
+with open('tests/test_data/test_jpg_200x300.jpg', 'rb') as buffer:
+    test_jpg_data: bytes = buffer.read()
+
+def test_load_from_url_should_return_image_object():
+    with patch('requests.get') as mock_get:
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.content = test_jpg_data
+        mock_get.return_value = mock_response
+
+        sut = ImageLoader()
+        actual = sut.load_from_url('http://somethingsomething.com/image.jpg')
+        assert isinstance(actual, Image.Image)
