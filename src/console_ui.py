0265955e4bb3738b40e03cdcdc99b0511667ede7
Jörgen Öberg <jo223cy@student.lnu.se>
2023-12-07 00:43:05 +0100

    GREEN

--- src/console_ui.py
+++ src/console_ui.py
@@ -1,5 +1,15 @@
 class ConsoleUI:
 
+  def start(self) -> None:
+    """
+    Start the console UI.
+    """
+    exit_message = 'Program exits. Bye!'
+    url = self.ask_for_url()
+    while url != 'q':
+      url = self.ask_for_url()
+    print(exit_message)
+
   def ask_for_url(self) -> str:
     """
     Ask the user for the URL of the image they want to analyze.
