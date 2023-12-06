7013153c338041a8c20ebe53c659e32a7e314175
Jörgen Öberg <jo223cy@student.lnu.se>
2023-12-07 00:24:05 +0100

    GREEN

--- src/console_ui.py
+++ src/console_ui.py
@@ -1,3 +1,7 @@
 class ConsoleUI:
 
-  pass
+  def ask_for_url(self) -> str:
+    """
+    Ask the user for the URL of the image they want to analyze.
+    """
+    return input('Enter the URL of the image you want to analyze: ')
