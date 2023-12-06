b978a19c52d938693d1038dacf2cd6de594dca2d
Jörgen Öberg <jo223cy@student.lnu.se>
2023-12-06 22:41:11 +0100

    Green

--- tests/test_image_loader.py
+++ tests/test_image_loader.py
@@ -17,3 +17,13 @@ def test_load_from_url_should_return_image_object():
         sut = ImageLoader()
         actual = sut.load_from_url('http://somethingsomething.com/image.jpg')
         assert isinstance(actual, Image.Image)
+
+def test_load_from_url_should_raise_exception_when_status_code_is_not_200():
+    with patch('requests.get') as mock_get:
+        mock_response = MagicMock()
+        mock_response.status_code = 404
+        mock_get.return_value = mock_response
+
+        sut = ImageLoader()
+        with pytest.raises(Exception):
+            sut.load_from_url('http://somethingsomething.com/image.jpg')
