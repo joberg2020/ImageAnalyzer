bf6d1f0a1ef6f245fce820dbeeba693fccbf8ac3
Jörgen Öberg <jo223cy@student.lnu.se>
2023-12-07 00:36:31 +0100

    RED

--- tests/test_console_ui.py
+++ tests/test_console_ui.py
@@ -14,3 +14,15 @@ def test_console_ui_ask_for_url_should_ask_for_url():
   mock_input.assert_called_once_with(expected)
   assert actual == expected
 
+def test_console_ui_start_should_ask_for_url_and_return_exitmessage_when_user_enters_q():
+  # Arrange
+  sut = ConsoleUI()
+  expected_exit_message = 'Program exits. Bye!'
+
+  # Act
+  with patch('builtins.input', return_value='q') as mock_input, patch('builtins.print') as mock_print:
+    sut.start()
+  
+  # Assert
+  mock_input.assert_called_once_with('Enter the URL of the image you want to analyze: ')
+  mock_print.assert_called_once_with(expected_exit_message)
