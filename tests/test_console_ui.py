529052a796327ad0edc87b8dc537b37551153db1
Jörgen Öberg <jo223cy@student.lnu.se>
2023-12-07 01:23:01 +0100

    REFACTOR

--- tests/test_console_ui.py
+++ tests/test_console_ui.py
@@ -1,11 +1,21 @@
+import pytest
 from src.console_ui import ConsoleUI
 from unittest.mock import Mock, patch
 
-def test_console_ui_ask_for_url_should_ask_for_url():
+@pytest.fixture
+def mock_image_loader():
+  return Mock()
+
+@pytest.fixture
+def mock_image_analyzer():
+  return Mock()
+
+@pytest.fixture
+def sut(mock_image_loader, mock_image_analyzer):
+  return ConsoleUI(mock_image_loader, mock_image_analyzer)
+
+def test_console_ui_ask_for_url_should_ask_for_url(sut):
   # Arrange
-  mock_image_loader = Mock()
-  mock_image_analyzer = Mock()
-  sut = ConsoleUI(mock_image_loader, mock_image_analyzer)
   expected = 'Enter the URL of the image you want to analyze: '
 
   # Act
@@ -16,11 +26,8 @@ def test_console_ui_ask_for_url_should_ask_for_url():
   mock_input.assert_called_once_with(expected)
   assert actual == expected
 
-def test_console_ui_start_should_ask_for_url_and_return_exitmessage_when_user_enters_q():
+def test_console_ui_start_should_ask_for_url_and_return_exitmessage_when_user_enters_q(sut):
   # Arrange
-  mock_image_loader = Mock()
-  mock_image_analyzer = Mock()
-  sut = ConsoleUI(mock_image_loader, mock_image_analyzer)
   expected_exit_message = 'Program exits. Bye!'
 
   # Act
@@ -31,14 +38,8 @@ def test_console_ui_start_should_ask_for_url_and_return_exitmessage_when_user_en
   mock_input.assert_called_once_with('Enter the URL of the image you want to analyze: ')
   mock_print.assert_called_once_with(expected_exit_message)
 
-def test_console_ui_constructor_should_recive_image_loader_and_image_analyzer():
-  # Arrange
-  mock_image_loader = Mock()
-  mock_image_analyzer = Mock()
-
-  # Act
-  sut = ConsoleUI(mock_image_loader, mock_image_analyzer)
+def test_console_ui_constructor_should_recive_image_loader_and_image_analyzer(sut):
 
   # Assert
-  assert sut.image_loader is mock_image_loader
-  assert sut.image_analyzer is mock_image_analyzer
+  assert isinstance(sut.image_loader, Mock)
+  assert isinstance(sut.image_analyzer, Mock)
